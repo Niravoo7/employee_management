c45242339af3e954b6327495a4464c27f366f113
Nirav Vaghani <niravvaghani555@gmail.com>
2025-04-04 11:23:00 +0530

    Refactored images

--- lib/core/constants/image_constants.dart
+++ lib/core/constants/image_constants.dart
@@ -9,10 +9,10 @@ class ImageConstants {
 /// Icon Constants
 class IconConstants {
   static const String rootIcon = "assets/images/icons";
-  static const String icBag = "$rootIcon/ic_bag.png";
-  static const String icCalendar = "$rootIcon/ic_calendar.png";
-  static const String icArrowRight = "$rootIcon/ic_arrow_right.png";
-  static const String icDelete = "$rootIcon/ic_delete.png";
+  static const String icBag = "$rootIcon/ic_bag.webp";
+  static const String icCalendar = "$rootIcon/ic_calendar.webp";
+  static const String icArrowRight = "$rootIcon/ic_arrow_right.webp";
+  static const String icDelete = "$rootIcon/ic_delete.webp";
 
   static const List<String> iconList = <String>[
     icBag,
